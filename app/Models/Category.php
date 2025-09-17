11a3aa7fa84f7fc4384029c830bd0e77c8357af4
Ali Naemi <ali.naemiorg@gmail.com>
2025-09-17 18:59:13 +0330

    feat(model): create section model and define relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
@@ -36,5 +37,12 @@ class Category extends Model
     public function children(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id');
+    }
+    /**
+     * The sections that belong to the category.
+     */
+    public function sections(): BelongsToMany
+    {
+        return $this->belongsToMany(Section::class);
     }
 }
